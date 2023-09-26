c94e650cbf2f8eb9b93dcfd7de7ca3ec8b4b9948
jovyllebermudez <jovyllebermudez@gmail.com>
2023-09-27 04:27:56 +0800

    handleselection use index to avoid search

--- src/components/menu/SelectionMenu.js
+++ src/components/menu/SelectionMenu.js
@@ -3,10 +3,10 @@ import React from "react";
 const SelectionMenu = ({ links, handleSelection }) => {
   return (
     <div className="flex overflow-x-scroll menu-container">
-      {links.map((value) => (
+      {links.map((value, index) => (
         <button
           key={value.title}
-          onClick={() => handleSelection(value.title)}
+          onClick={() => handleSelection(index)}
           className="text-white bg-violet-500 p-1 px-2 rounded-md ml-3"
         >
           {value.title}
