d520c2be6591386ddbc94a857907125fca88479c
Kunal7069 <107999879+Kunal7069@users.noreply.github.com>
2023-10-04 11:23:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import MainSection from "./MainSection";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MainSection from './MainSection'
+import { Provider } from 'react-redux';
+import store from './utils/store';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <>
       <MainSection />
+    
     </>
-  );
-};
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>)
