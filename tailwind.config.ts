ed8bcc8442bcb7df9871b3006e822df8d0051b58
Gilson Oliveira <juninhokaponnemg@hotmail.com>
2023-10-04 19:24:47 -0300

    refactor: configured tailwind to support ts, tsx files

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      height: {
+        '128': '37rem',
+      },
+    },
+  },
+  plugins: [],
+};
