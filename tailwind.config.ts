bee67365ac5de5024b8b7ebe206b07b0a8501725
Igor Tadić <97162709+thecodereule@users.noreply.github.com>
2023-10-06 17:34:55 +0000

    Lowered the intensity of Blur on hover

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      backdropBlur: {
+        xs : '1.5px',
+      },
       height: {
         '128': '37rem',
       },
