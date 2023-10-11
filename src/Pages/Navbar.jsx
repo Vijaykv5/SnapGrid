e698683eac9b6b64e77595bc4b197ecc83c7a315
unknown <jayprakashpj2525@gmail.com>
2023-10-11 23:08:56 +0530

    Added Home Page

---
+++ src/Pages/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import user from '../components/assets/User.png';
+import cam from '../components/assets/cam.png';
+
+export default function Navbar() {
+  return (
+    <nav className='py-8 px-12 flex items-center justify-between'>
+      <div className='flex items-center'>
+        <img src={cam} alt='cam.png' className='w-12 lg:w-[4.5rem]' />
+        <p className='text-2xl lg:text-[32px] text-[#FFFFFF] font-semibold'>
+          SnapGrid
+        </p>
+      </div>
+      <div className='hidden md:block'>
+        <ul className='flex space-x-10 text-[16px] text-[#FFFFFF] items-center'>
+          <li className='cursor-pointer'>Docs</li>
+          <li className='cursor-pointer'>Github</li>
+          <li>
+            <button
+              className='rounded-[1.25rem] px-8 py-2 flex items-center font-semibold'
+              style={{
+                background: 'linear-gradient(270deg, #6F4FF2 0%, #6F4FF2 100%)',
+              }}
+            >
+              <img src={user} className='pr-3' alt='user.png' />
+              Sign Up
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
