6da42cd30e162f1797c6b86fe3f728c93bed2b36
rojonaitor <bryanlundberg@outlook.com>
2023-10-26 13:43:02 -0600

    style: homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,109 +1,109 @@
 import React from 'react';
-import { useEffect,useState,useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-import rocket from '../components/assets/RocketLaunch.png';
-import bg from '../components/assets/bg.png';
-import nft from '../components/assets/nft.png';
-import rectangle from '../components/assets/rectangle.png';
-import Navbar from './Navbar';
-import '../utils/style.css';
 
+import rocket from '../components/assets/RocketLaunch.png';
+import a from '../images/a.png';
+import b from '../images/b.png';
+import c from '../images/c.png';
+import d from '../images/d.png';
+import '../utils/style.css';
+import Navbar from './Navbar';
 
 export default function HomePage() {
-
   // Animation in home Page
   const [isVisible, setIsVisible] = useState(false);
   const ref = useRef(null);
   const ref2 = useRef(null);
   const ref3 = useRef(null);
-  const ref4 = useRef(null);
-
 
   // Multiple Ref to animate multiple elements [II]
   useEffect(() => {
-    if(isVisible){
+    if (isVisible) {
       ref2.current.classList.add('slide__in2');
       ref.current.classList.add('slide__in');
       ref3.current.classList.add('slide__in3');
-      ref4.current.classList.add('slide__in');
-    }
-    else{
+    } else {
       ref.current.classList.remove('slide__in');
       ref2.current.classList.remove('slide__in2');
       ref3.current.classList.remove('slide__in3');
-      ref4.current.classList.remove('slide__in');
     }
-  })
-
+  });
 
   // Intersection Observer [I]
   useEffect(() => {
-    const observer= new IntersectionObserver(([entry])=>{
+    const observer = new IntersectionObserver(([entry]) => {
       setIsVisible(entry.isIntersecting);
       console.log(entry.isIntersecting);
-    })
+    });
     observer.observe(ref.current);
-    return ()=> observer.disconnect();
-},[]);
+    return () => observer.disconnect();
+  }, []);
 
   return (
-    <div
-      style={{
-        background:
-          'linear-gradient(31.67deg, rgba(3, 14, 21, 0.96) -2.58%, #05121B 70.47%)',
-      }}
-      className='h-screen overflow-y-scroll'
-    >
+    <div className='flex flex-col max-h-screen min-h-screen overflow-auto bg-gradient-to-r from-slate-950 to-gray-900'>
       <Navbar />
 
-      <div  className=' relative lg:flex-row pt-12 md:pt-20 space-y-14 lg:space-y-0  px-6 flex flex-col-reverse lg:px-20 py-8 justify-between'>
-        <div className='hidden lg:block absolute top-0 right-0'>
-          <img src={bg} className='h-48 w-80' />
-        </div>
+      <div className='relative px-6 py-8 pt-12 grow'>
         {/* headline and subhead... */}
-        <div  className='flex flex-col space-y-10  '>
-          <div className='home__image' ref={ref}>
-            <h1 className='text-[#FFFFFF] text-4xl md:text-6xl lg:text-7xl font-semibold'>
-              Discover.Explore.
+        <div className='flex flex-col items-center space-y-6 grow'>
+          <div className='flex flex-col items-center justify-center w-full'>
+            <h1
+              ref={ref}
+              className='inline-block font-medium text-center text-transparent md:text-7xl text-5xl lg:text-8xl balance bg-gradient-to-r from-white to-neutral-400 bg-clip-text lg:max-w-[900px] md:max-w-[700px] w-4/5 sm:max-w-full m-0'
+            >
+              Discover
             </h1>
-            <p className='text-[#FFFFFF] text-4xl md:text-6xl lg:text-7xl font-semibold'>
-              Share.
-            </p>
+            <h1
+              ref={ref}
+              className='inline-block font-medium text-center text-transparent md:text-7xl text-5xl lg:text-8xl balance bg-gradient-to-r from-white to-neutral-400 bg-clip-text lg:max-w-[900px] md:max-w-[700px] w-4/5 sm:max-w-full m-0'
+            >
+              Explore & Share
+            </h1>
           </div>
-          <p  ref={ref2} className='home__image text-[#FFFFFF]  text-lg md:text-xl leading-8 max-w-2xl'>
-            <span className='text-[#6F4FF2] font-normal '>SnapGrid</span> is a
-            versatile web platform that simplifies image discovery and
-            downloads. Users can effortlessly find and acquire high-quality
-            images spanning various categories, from nature to architecture.
-          </p>
-          <div ref={ref3} className='home__image'>
-            <Link to={'/search'}>
-              <button
-                style={{
-                  background:
-                    'linear-gradient(270deg, #6F4FF2 0%, #6F4FF2 100%)',
-                }}
-                className='rounded-2xl px-10 py-3 flex items-center text-[#FFFFFF]'
-              >
-                <img  className='pr-3 ' src={rocket} alt='' />
-                Explore
-              </button>
-            </Link>
-          </div>
-        </div>
 
-        {/* Image ... */}
-        <div className='home__image' ref={ref4}>
-          <img
-            
-            src={nft}
-            className=' max-w-xs md:max-w-md lg:max-w-sm'
-            alt='img.png'
-          />
+          <p
+            ref={ref2}
+            className='w-4/5 max-w-2xl leading-8 text-center text-md text-neutral-400 md:text-xl'
+          >
+            Used by some of the world's largest companies,{' '}
+            <span className='font-normal text-center text-white'>
+              SnapGrid simplifies image discovery and downloads
+            </span>
+            . Users can easily find and acquire high-quality images spanning
+            various categories.
+          </p>
+
+          <div className='grid grid-cols-2 md:grid-cols-4 overflow-hidden rounded-lg max-w-[800px] sm:max-w-4/5'>
+            <img
+              src={a}
+              alt='random image'
+              className='object-cover w-full h-32 md:h-40'
+            />
+            <img
+              src={b}
+              alt='random image'
+              className='object-cover w-full h-32 md:h-40'
+            />
+            <img
+              src={c}
+              alt='random image'
+              className='object-cover w-full h-32 md:h-40'
+            />
+            <img
+              src={d}
+              alt='random image'
+              className='object-cover w-full h-32 md:h-40'
+            />
+          </div>
+
+          <Link to={'/search'} ref={ref3}>
+            <button className='flex items-center px-10 py-3 transition duration-300 bg-violet-800 text-neutral-200 hover:text-neutral-100 hover:bg-violet-600 rounded-2xl '>
+              <img className='pr-3 ' src={rocket} alt='' />
+              Explore Now!
+            </button>
+          </Link>
         </div>
-      </div>
-      <div className=''>
-        <img src={rectangle} className='' />
       </div>
     </div>
   );
