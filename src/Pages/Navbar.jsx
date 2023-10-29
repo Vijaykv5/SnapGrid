f853a136677652c831665566073e3849dbdcbcf7
Aasim Taif <taifullahansari@gmail.com>
2023-10-29 21:51:25 +0530

    changed route from signUp to signup

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -28,7 +28,7 @@ export default function Navbar() {
             <Link to={'/docs'}><li className='cursor-pointer font-semibold text-lg'>Docs</li></Link>
             <a href='https://github.com/Vijaykv5/SnapGrid' target='_blank'><li className='cursor-pointer font-semibold text-lg'>Github</li></a>
             <li>
-              <Link to='/signUp'>
+              <Link to='/signup'>
                 <button
                   className='rounded-[1.25rem] px-8 py-2 flex items-center font-semibold'
                   style={{
@@ -48,7 +48,7 @@ export default function Navbar() {
         <div className={`flex justify-evenly items-center flex-col ${visibility ? "visible__drawer__container" : "hidden__drawer__container"} `}>
           <a href='/docs' className=' font-bold text-white text-2xl m-2'>Docs</a>
           <a href='https://github.com/Vijaykv5/SnapGrid' className='font-bold text-white text-2xl m-2 '>Github</a>
-          <Link to='/signUp'>
+          <Link to='/signup'>
             <button
               className='rounded-[1.25rem] px-8 py-2 flex items-center font-semibold text-white m-2 '
               style={{
