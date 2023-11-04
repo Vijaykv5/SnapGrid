7e2aee5969684909a41a8e836edb0bcdedc98432
JavidSumra <javidsumra135@gmail.com>
2023-11-04 16:12:56 +0530

    Adding Feature of PWA

--- src/components/menu/HamburgerMenu.tsx
+++ src/components/menu/HamburgerMenu.tsx
@@ -1,20 +1,32 @@
-import React from "react"
+import React from 'react';
 
-interface Props{
-    onClick:()=>void,
-    visibility:Boolean
+interface Props {
+  onClick: () => void;
+  visibility: boolean;
 }
 
-const HamburgerMenu : React.FC<Props> =({onClick,visibility})=>{
-    console.log(visibility)
-    return(
-        <div className='flex w-10 h-10 flex-col items-center justify-evenly sm:visible xs:visible text-white hamburger' onClick={onClick} >
-        
-            <div className={`w-10 h-1 bg-white ${visibility?"hamburger__1":"hamburger_1"} `}  ></div>
-            <div className={`w-10 h-1 bg-white ${visibility? "hamburger__2":"hamburger_1"} `} ></div>
-            <div className={`w-10 h-1 bg-white ${visibility?"hamburger__3":"hamburger_1"}`}></div>
-
-        </div>
-    )
-}
-export default HamburgerMenu
+const HamburgerMenu: React.FC<Props> = ({ onClick, visibility }) => {
+  return (
+    <div
+      className='flex w-10 h-10 flex-col items-center justify-evenly sm:visible xs:visible text-white hamburger'
+      onClick={onClick}
+    >
+      <div
+        className={`w-10 h-1 bg-white ${
+          visibility ? 'hamburger__1' : 'hamburger_1'
+        } `}
+      ></div>
+      <div
+        className={`w-10 h-1 bg-white ${
+          visibility ? 'hamburger__2' : 'hamburger_1'
+        } `}
+      ></div>
+      <div
+        className={`w-10 h-1 bg-white ${
+          visibility ? 'hamburger__3' : 'hamburger_1'
+        }`}
+      ></div>
+    </div>
+  );
+};
+export default HamburgerMenu;
